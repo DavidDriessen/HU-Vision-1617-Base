2fcc9d083809cd7c1dba9128778ae5b8d234470f
MSI\david <david.driessen@student.hu.nl>
2017-04-14 15:01:55 +0200

    documentatie

--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -1,49 +1,59 @@
 #include "RGBImageStudent.h"
 
+// Make an empty image
 RGBImageStudent::RGBImageStudent() : RGBImage() {
 	img = new RGB[1];
 	max = 0;
 }
 
+// Clone image
 RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()) {
 	set(other.getWidth(), other.getHeight());
 }
 
-
+// Make an new image with size
 RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height) {
 	set(width, height);
 }
 
+// Remove image from memory
 RGBImageStudent::~RGBImageStudent() {
 	delete[] img;
 }
 
+// Set image size and assign memory
 void RGBImageStudent::set(const int width, const int height) {
-	delete[] img;
+	if (img != nullptr) {
+		delete[] img;
+	}
 	img = new RGB[width * height];
 	max = width * height - 1;
 }
 
+// Set image
 void RGBImageStudent::set(const RGBImageStudent &other) {
 	set(other.getWidth(), other.getHeight());
 }
 
+// Set pixel value by coordinates
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
 	setPixel(x + getWidth() * y, pixel);
 }
 
+// Set pixel value by index
 void RGBImageStudent::setPixel(int i, RGB pixel) {
-
 	if (i > max) {
 		return;
 	}
 	img[i] = pixel;
 }
 
+// Get pixel value by coordinates
 RGB RGBImageStudent::getPixel(int x, int y) const {
 	return getPixel(x + getWidth() * y);
 }
 
+// Get pixel value by index
 RGB RGBImageStudent::getPixel(int i) const {
 	if (i > max) {
 		return -1;
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -2,13 +2,13 @@
 #include "ImageFactory.h"
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	IntensityImage * img = ImageFactory::newIntensityImage();
-	img->set(image.getWidth(), image.getHeight());
-	int max = image.getWidth() * image.getHeight();
-	for (int i = 0; i < max; i++) {
-		img->setPixel(i, (image.getPixel(i).r + image.getPixel(i).g + image.getPixel(i).b)/3);
+	IntensityImage * img = ImageFactory::newIntensityImage(); // Get Image too fill
+	img->set(image.getWidth(), image.getHeight());	// Set GrayScale image size
+	int max = image.getWidth() * image.getHeight();	// Calculate position of last pixel
+	for (int i = 0; i < max; i++) { // Loop through all pixels
+		img->setPixel(i, (image.getPixel(i).r + image.getPixel(i).g + image.getPixel(i).b)/3); // Set pixel intensity by getting the avarage of the color channels
 	}
-	return img;
+	return img; // Return the GrayScale image
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
